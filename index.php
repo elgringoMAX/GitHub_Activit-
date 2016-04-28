e5f5a1bf0f29e885a841afe2141a129bfb5ea1b0
elgringoMAX <m.bute@hotmail.fr>
2016-04-28 11:51:48 +0200

    code source

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="newcss.css" rel="stylesheet" type="text/css"/>
+        <title></title>
+    </head>
+    <body>
+        <div class="container">
+        <div id="sort1" class="ui cards" data-sortable='.card' >
+            <div data-position="0" data-id="1" class="card">
+                <div class="content"> <img class="photo" src="myAvatar.png"> <center><strong> Devellopeur WEB junior chez SIMPLON </br></br> Technologie </strong><ul> <li>HTML 5/CSS 3</li> <li>JavaScript</li> <li>PHP</li></ul></center></div>
+            </div>
+            
+            <div data-position="1" data-id="2" class="card">
+                <div class="content"> <img class="photo" src="avatar.png"> <center><strong> Devellopeur WEB junior chez SIMPLON </br></br> Technologie </strong><ul> <li>HTML 5/CSS 3</li> <li>JavaScript</li> <li>PHP</li></ul></center></div>
+            </div>
+            
+            <div data-position="2" data-id="3" class="card">
+                <div class="content"> <img class="photo" src="avatarMag.png"> <center><strong> Devellopeur WEB junior chez SIMPLON </br></br> Technologie </strong><ul> <li>HTML 5/CSS 3</li> <li>JavaScript</li> <li>PHP</li></ul></center></div>
+            </div>
+            
+             <div data-position="3" data-id="4" class="card">
+                 <div class="content"> <img class="photo" src="avatarJames.jpg"> <center><strong> Devellopeur WEB junior chez SIMPLON </br></br> Technologie </strong><ul> <li>HTML 5/CSS 3</li> <li>JavaScript</li> <li>PHP</li></ul></center></div>
+            </div>
+            
+             <div data-position="4" data-id="5" class="card">
+                <div class="content"> <img class="photo" src="avatarMax.jpg"> <center><strong> Devellopeur WEB junior chez SIMPLON </br></br> Technologie </strong><ul> <li>HTML 5/CSS 3</li> <li>JavaScript</li> <li>PHP</li></ul></center></div>
+            </div>
+            
+             <div data-position="5" data-id="6" class="card">
+                <div class="content"> <img class="photo" src="myAvatar (2).png"> <center><strong> Devellopeur WEB junior chez SIMPLON </br></br> Technologie </strong><ul> <li>HTML 5/CSS 3</li> <li>JavaScript</li> <li>PHP</li></ul></center></div>
+            </div>
+            
+            <div data-position="6" data-id="7" class="card">
+                <div class="content"> <img class="photo" src="myAvatar (3).png"> <center><strong> Devellopeur WEB junior chez SIMPLON </br></br> Technologie </strong><ul> <li>HTML 5/CSS 3</li> <li>JavaScript</li> <li>PHP</li></ul></center></div>
+            </div>
+            
+            <div data-position="7" data-id="8" class="card">
+                <div class="content"> <img class="photo" src="myAvatar (4).png"> <center><strong> Devellopeur WEB junior chez SIMPLON </br></br> Technologie </strong><ul> <li>HTML 5/CSS 3</li> <li>JavaScript</li> <li>PHP</li></ul></center></div>
+            </div>
+            
+            <div data-position="8" data-id="9" class="card">
+                <div class="content"> <img class="photo" src="myAvatar (5).png"> <center><strong> Devellopeur WEB junior chez SIMPLON </br></br> Technologie </strong><ul> <li>HTML 5/CSS 3</li> <li>JavaScript</li> <li>PHP</li></ul></center></div>
+            </div>
+            
+            <div data-position="9" data-id="10" class="card">
+                <div class="content"> <img class="photo" src="myAvatar (6).png"> <center><strong> Devellopeur WEB junior chez SIMPLON </br></br> Technologie </strong><ul> <li>HTML 5/CSS 3</li> <li>JavaScript</li> <li>PHP</li></ul></center></div>
+            </div>
+            
+            <div data-position="10" data-id="11" class="card">
+                <div class="content"> <img class="photo" src="myAvatar (7).png"> <center><strong> Devellopeur WEB junior chez SIMPLON </br></br> Technologie </strong><ul> <li>HTML 5/CSS 3</li> <li>JavaScript</li> <li>PHP</li></ul></center></div>
+            </div>
+            
+            <div data-position="11" data-id="12" class="card">
+                <div class="content"> <img class="photo" src="myAvatar (8).png"> <center><strong> Devellopeur WEB junior chez SIMPLON </br></br> Technologie </strong><ul> <li>HTML 5/CSS 3</li> <li>JavaScript</li> <li>PHP</li></ul></center></div>
+            </div>
+            
+            <div data-position="12" data-id="13" class="card">
+                <div class="content"> <img class="photo" src="myAvatar (9).png"> <center><strong> Devellopeur WEB junior chez SIMPLON </br></br> Technologie </strong><ul> <li>HTML 5/CSS 3</li> <li>JavaScript</li> <li>PHP</li></ul></center></div>
+            </div>
+            
+            
+        </div>
+        </div>
+      
+        <script src="interactjs.js" type="text/javascript"></script>
+        <script src="newjavascript.js" type="text/javascript"></script>
+        <script> sortable = new Sortable(document.querySelector('#sort1'));
+        </script>
+    </body>
+</html>
